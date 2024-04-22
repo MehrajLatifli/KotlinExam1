3c179eb606fbdcc8853ec507aeed896322cb4660
MehrajLatifli <meracletifli@gmail.com>
2024-04-22 20:42:00 +0400

    GOD bless universes

--- src/main/kotlin/Task5/Task5.kt
+++ src/main/kotlin/Task5/Task5.kt
@@ -1,27 +1,28 @@
 package Task5
 
 fun main() {
-    val array = arrayListOf<Int>(1, 2, 3, 2, 5, 2, 44)
+    val array = arrayListOf<Int>(1, 4, 4, 2, 5, 2, 4,18,29)
 
     print("\nArray elements: ")
     for ((index, item) in array.withIndex()) {
         print(item.toString()+" ")
     }
 
-    print("\nRepeated numbers and their counts: ")
-    val countMap = hashMapOf<Int, Int>()
+    print("\n\nRepeated numbers and their counts: ")
+    val countMap = HashMap<Int, Int>()
     for (item in array) {
         countMap[item] = countMap.getOrDefault(item, 0) + 1
     }
 
     for ((number, count) in countMap) {
         if (count > 1) {
-            print("$number - $count times")
+            print("\n $number -> $count times ")
+
         }
     }
 
-    val printedSet = hashSetOf<Int>()
-    print("\nArray elements without repeats: ")
+    val printedSet = HashSet<Int>()
+    print("\n\nArray elements without repeats: ")
     for ((index, item) in array.withIndex()) {
         if (printedSet.add(item)) {
             print(item.toString() + " ")
